4af15ccb4f4089f2b8962024111b79e33b2d5ea4
prena2712 <prernagupta2712@gmail.com>
2021-07-07 20:27:26 +0530

    removed pusher and turn

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -66,16 +66,16 @@ const Room = (props) => {
 
     function createPeer(userID) {
         const peer = new RTCPeerConnection({
-            trickel: false,
+            // trickel: false,
             iceServers: [
                 {
                     urls: "stun:stun.stunprotocol.org"
-                },
-                {
-                    url: 'turn:192.158.29.39:3478?transport=udp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                },
+                }
+                // {
+                //     url: 'turn:192.158.29.39:3478?transport=udp',
+                //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                //     username: '28224511:1379330808'
+                // },
             ]
         });
 
